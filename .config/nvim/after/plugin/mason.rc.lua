d7491ea776d6106c2d5cf51ca1895e0e273df7aa
Moryoka <55505135+MoryokaV@users.noreply.github.com>
2022-08-16 17:51:45 +0300

    Update mason.rc.lua

--- .config/nvim/after/plugin/mason.rc.lua
+++ .config/nvim/after/plugin/mason.rc.lua
@@ -5,21 +5,9 @@ if (not status2) then return end
 
 mason.setup {}
 lspconfig.setup {
-  ensure_installed = { 'clangd', 'pylsp', 'tailwindcss', 'tsserver' }
+  ensure_installed = { 'clangd', 'tailwindcss', 'tsserver' }
 }
 
 require'lspconfig'.clangd.setup{}
-require'lspconfig'.pylsp.setup{
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-          ignore = {'W391', 'W291', 'W293', 'E302'},
-          maxLineLength = 100
-        }
-      }
-    }
-  }
-}
 require'lspconfig'.tsserver.setup{}
 require'lspconfig'.tailwindcss.setup{}
